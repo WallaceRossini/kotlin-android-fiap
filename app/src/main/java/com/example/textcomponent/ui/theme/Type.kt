ddea939a8dfe5f761063e54d88e94dbd0c7a475c
Wallace Rossini <rossiniwallace@gmail.com>
2023-08-18 00:44:38 -0300

    feat: inclusão de fontes no projeto

--- app/src/main/java/com/example/textcomponent/ui/theme/Type.kt
+++ app/src/main/java/com/example/textcomponent/ui/theme/Type.kt
@@ -2,10 +2,13 @@ package com.example.textcomponent.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.textcomponent.R
 
+val Kablammo = FontFamily(Font(R.font.kablammo_regular))
 // Set of Material typography styles to start with
 val Typography = Typography(
         bodyLarge = TextStyle(
